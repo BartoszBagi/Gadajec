726b0f724d2c14608126088608f51aa870139f54
Bartosz <bbaginski93@gmail.com>
2022-12-03 09:27:44 +0100

    Rejestracja i logowanie update

--- Gadajec/Gadajec/Gadajec.Client/Program.cs
+++ Gadajec/Gadajec/Gadajec.Client/Program.cs
@@ -3,13 +3,18 @@ using Gadajec.Client;
 using Gadajec.Client.Brokers.API;
 using Gadajec.Client.Service.Authentication;
 using GadajecBlazor.Providers;
+using Majorsoft.Blazor.Components.CssEvents;
 using Majorsoft.Blazor.Components.Notifications;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
+using System.Globalization;
+
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("#wrapper");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
@@ -25,7 +30,20 @@ builder.Services.AddScoped<ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider>(p =>
                 p.GetRequiredService<ApiAuthenticationStateProvider>());
 
+builder.Services.AddCssEvents();
 builder.Services.AddNotifications();
 //builder.Services.AddLocalization();
 
-await builder.Build().RunAsync();
+var host = builder.Build();
+
+var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+
+var result = await jsInterop.InvokeAsync<string>("blazorCulture.get");
+if (result != null)
+{
+    var culture = new CultureInfo(result);
+    CultureInfo.DefaultThreadCurrentCulture = culture;
+    CultureInfo.DefaultThreadCurrentUICulture = culture;
+}
+
+await host.RunAsync();
